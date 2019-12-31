439ea3e65ccaba90c0c0bfc69a3e69a7f7a392b4
Thomas Humphrey <thumphrey9123@gmail.com>
2019-12-31 01:04:07 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -13,42 +13,8 @@ end
 def reduce(source_array, starting_point = 0)
   i=0 
   while i<source_array.length do
-    yield(source_array[i])
+    starting_point = yield(starting_point, source_array[i])
     i+= 1
   end
   starting_point
-end
-
-
-
-
-def reduce_to_total(source_array, starting_point = 0)
-  i=0 
-  while i<source_array.length do
-    starting_point+= source_array[i]
-    i+= 1
-  end
-  starting_point
-end
-
-def reduce_to_all_true(source_array)
-  i=0 
-  while i<source_array.length do
-    if source_array[i] == false
-      return false
-    end
-    i+= 1 
-  end
-  true
-end
-
-def reduce_to_any_true(source_array)
-  i=0 
-  while i<source_array.length do
-    if source_array[i] == true
-      return true
-    end
-    i+= 1 
-  end
-  false
 end
